85f43b53ccc7dc168534ccd4c774a9ce961b5811
Jason Ling <jtg2078@gmail.com>
2012-03-18 11:05:39 +0000

    completed hw3-part2b

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -30,4 +30,16 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  # tmd = "ratings_R"
+  # step %Q{I check "#{tmd}"}
+  rating_list.split(',').each do |rating|
+  	# And I check "ratings_R"
+  	# And I uncheck "ratings_G"
+  	# And I uncheck "ratings_PG-13"
+  	if uncheck.nil?
+  		step %Q{I check "#{("ratings_"+ rating.strip)}"}
+  	else
+  		step %Q{I uncheck "#{("ratings_"+ rating.strip)}"}
+  	end
+  end
 end
